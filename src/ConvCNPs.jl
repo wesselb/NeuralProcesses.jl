b99fe4d910a812d5d548077993a6f1132dd74e81
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-03-17 19:59:28 +0100

    Include data.jl

--- src/ConvCNPs.jl
+++ src/ConvCNPs.jl
@@ -1,15 +1,19 @@
 module ConvCNPs
 
+using Distributions
 using Flux
 using Flux.Tracker
+using NNlib
 using Printf
 using Random
 using Statistics
+using Stheno
 
 include("util.jl")
 include("discretisation.jl")
 include("setconv.jl")
 include("conv.jl")
 include("model.jl")
+include("data.jl")
 
 end
