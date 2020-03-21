f3b22edfc2ee7555d74ef551331ddc3dafcb8394
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-03-21 16:45:51 +0100

    Fix train script for GPU

--- train.jl
+++ train.jl
@@ -6,49 +6,35 @@ Pkg.instantiate()
 
 using ConvCNPs
 using Flux
-using GPUArrays
 using Random
 using Stheno
 using StatsBase
 using Distributions
 using Plots
 
-GPUArrays.allowscalar(false)
-
-
 function plot_task(model, epoch)
     x = LinRange(-3, 3, 400)
     task = 1
 
-    batch = gpu(data_gen(1)[1])
-    y_mean, y_std = model(batch.x_context, batch.y_context, repeat(x, 1, 1, 16))
-    y_mean = y_mean[:, 1, task].data
-    y_std = y_std[:, 1, task].data
+    # Extract a task.
+    x_context, y_context, x_target, y_target = map(x -> cpu(x[:, 1, task]), data_gen(1)[1])
+
+    # Run model. Take care of the dimensionality of all objects.
+    expand(x) = reshape(x, length(x), 1, 1)
+    y_mean, y_std = map(
+        x -> Flux.data(cpu(x[:, 1, 1])),
+        model(expand.((x_context, y_context, x_target))...)
+    )
 
     plt = plot()
 
     # Scatter context set
-    scatter!(
-        plt,
-        batch.x_context[:, 1, task],
-        batch.y_context[:, 1, task],
-        c=:black,
-        label="Context set"
-    )
-    scatter!(
-        plt,
-        batch.x_target[:, 1, task],
-        batch.y_target[:, 1, task],
-        c=:red,
-        label="Target set"
-    )
+    scatter!(plt, x_context, y_context, c=:black, label="Context set")
+    scatter!(plt, x_target, y_target, c=:red, label="Target set")
 
     # Plot prediction
     plot!(plt, x, y_mean, c=:green, label="Model Output")
-    plot!(
-        plt,
-        x,
-        [y_mean y_mean],
+    plot!(plt, x, [y_mean y_mean],
         fillrange=[y_mean .+ 2y_std y_mean .- y_std],
         fillalpha=0.2,
         c=:green,
@@ -59,36 +45,18 @@ function plot_task(model, epoch)
     savefig(plt, "output/epoch$epoch.pdf")
 end
 
-
 function loss(model, x_context, y_context, x_target, y_target)
-    return -mean(gaussian_logpdf(
-        y_target,
-        model(x_context, y_context, x_target)...
-    ))
+    return -mean(gaussian_logpdf(y_target, model(x_context, y_context, x_target)...))
 end
-
-
-function data_to_gpu(data)
-    return [(
-        x_context=gpu(Array{Float32}(x.x_context)),
-        y_context=gpu(Array{Float32}(x.y_context)),
-        x_target=gpu(Array{Float32}(x.x_target)),
-        y_target=gpu(Array{Float32}(x.y_target))
-    ) for x in data]
-end
-
 
 function eval_model(model; num_batches=16)
-    data = data_to_gpu(data_gen(num_batches))
-    res = mean(map(x -> loss(model, x...), data))
-    loss_value = Flux.data(res)
+    loss_value = Flux.data(mean(map(x -> loss(model, x...), data_gen(num_batches))))
     println("Test loss: $loss_value ($num_batches batches)")
 end
 
-
 # Construct data generator.
-scale = 0.25
-k = stretch(matern52(), 1 / scale)
+scale = 0.25f0
+k = stretch(matern52(), 1 / Float64(scale))  # Use `Float64`s for the data generation.
 data_gen = DataGenerator(
     k;
     batch_size=16,
@@ -106,16 +74,15 @@ conv = Chain(
     Conv((5, 1), 16=>8, pad=(2, 0), relu; init=Flux.glorot_normal),
     Conv((1, 1), 8=>2, pad=0; init=Flux.glorot_normal),
 )
-arch = (conv=conv, points_per_unit=32, multiple=1)
+arch = (conv=conv, points_per_unit=32f0, multiple=1)
 
 # Use an architecture with depthwise separable convolutions.
 # arch = build_conv(scale * 2, 4, 8)
 
 # Instantiate ConvCNP model.
-model = convcnp_1d(arch; margin = scale * 2)
+model = convcnp_1d(arch; margin = scale * 2) |> gpu
 
-# Move to GPU and evaluate once.
-model = model |> gpu
+# Evaluate once before training.
 eval_model(model)
 
 # Configure training.
@@ -128,11 +95,11 @@ for epoch in 1:EPOCHS
     Flux.train!(
         (xs...) -> loss(model, xs...),
         Flux.params(model),
-        data_to_gpu(data_gen(TASKS_PER_EPOCH)),
+        data_gen(TASKS_PER_EPOCH),
         opt,
         cb = Flux.throttle(() -> eval_model(model), 10)
     )
     println("Epoch done")
     eval_model(model; num_batches=128)
-    # plot_task(model, epoch)
+    plot_task(model, epoch)
 end
