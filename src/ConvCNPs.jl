3728bddaa02a749fb68462a85896f737f637d143
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-03-21 16:41:09 +0100

    Disallow scalar computations on the GPU

--- src/ConvCNPs.jl
+++ src/ConvCNPs.jl
@@ -2,11 +2,14 @@ module ConvCNPs
 
 using Distributions
 using Flux
+using GPUArrays
 using NNlib
 using Printf
 using Random
 using Statistics
 using Stheno
+
+GPUArrays.allowscalar(false)
 
 include("util.jl")
 include("discretisation.jl")
