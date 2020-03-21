fd9cb09ce1b46f86bc5a0014710fbc4e713472a2
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-03-21 16:41:19 +0100

    Specialise to Float32, the 1D case, and fix init

--- src/conv.jl
+++ src/conv.jl
@@ -1,9 +1,9 @@
-export Architecture, build_conv
+export Architecture, build_conv_1d
 
 const Architecture = NamedTuple{
     (:conv, :points_per_unit, :multiple),
-    Tuple{U, S, T}
-} where U<:Chain where S<:Real where T<:Integer
+    Tuple{S, Float32, T}
+} where S<:Chain where T<:Integer
 
 function _compute_kernel_size(receptive_field, points_per_unit, num_layers)
     receptive_points = receptive_field * points_per_unit
@@ -14,68 +14,67 @@ _compute_padding(kernel_size) = Integer(floor(kernel_size / 2))
 
 """
     build_conv(
-        receptive_field,
-        num_layers,
-        num_channels;
-        points_per_unit=64,
-        multiple=1,
-        in_channels=2,
-        out_channels=1,
-        dimensionality=1,
+        receptive_field::Float32,
+        num_layers::Integer,
+        num_channels::Integer;
+        points_per_unit::Float32=64f0,
+        multiple::Integer=1,
+        in_channels::Integer=2,
+        out_channels::Integer=2
     )
 
-Build a CNN with a specified receptive field size.
+Build a 1D CNN with a specified receptive field size.
 
 # Arguments
-- `receptive_field::Real`: Width of the receptive field.
+- `receptive_field::Float32`: Width of the receptive field.
 - `num_layers::Integer`: Number of layers of the CNN.
 - `num_channels::Integer`: Number of channels of the CNN.
 
 # Keywords
-- `points_per_unit::Real=64`: Points per unit for the discretisation. See
+- `points_per_unit::Float32=64f0`: Points per unit for the discretisation. See
      `UniformDiscretisation1d`.
 - `multiple::Integer=1`: Multiple for the discretisation. See `UniformDiscretisation1d`.
 - `in_channels::Integer=2`: Number of input channels.
-- `out_channels::Integer=1`: Number of output channels.
-- `dimensionality::Integer=1`: Dimensionality of the inputs. One corresponds to time series
-    and two corresponds to images.
+- `out_channels::Integer=2`: Number of output channels.
 
 # Returns
 - `Architecture`: Corresponding CNN bundled with the specified points per unit and margin.
 """
-function build_conv(
-    receptive_field::Real,
+function build_conv_1d(
+    receptive_field::Float32,
     num_layers::Integer,
     num_channels::Integer;
-    points_per_unit::Real=64,
+    points_per_unit::Float32=64f0,
     multiple::Integer=1,
     in_channels::Integer=2,
     out_channels::Integer=2,
-    dimensionality::Integer=1
 )
-    kernel_size = _compute_kernel_size(receptive_field, points_per_unit, num_layers)
-    padding = _compute_padding(kernel_size)
-
-    # Repeat the kernel size `dimensionality` many times to construct the
-    # convolution kernel.
-    kernel_pointwise = ntuple(_ -> 1, dimensionality)
-    kernel = ntuple(_ -> kernel_size, dimensionality)
-
-    kernel_pointwise = (1, 1)
-    kernel = (kernel_size, 1)
+    # We use two-dimensional kernels: CUDNN does not support 1D convolutions.
+    kernel = (_compute_kernel_size(receptive_field, points_per_unit, num_layers), 1)
+    padding = (_compute_padding(kernel_size), 0)
 
     # Build layers of the conv net.
     layers = []
-    push!(layers, Conv(kernel_pointwise, in_channels=>num_channels, relu))
+    push!(layers, Conv(
+        (1, 1),
+        in_channels=>num_channels,
+        relu;
+        init=Flux.glorot_normal
+    ))  # Pointwise matrix multiplication
     for i = 1:(num_layers - 2)
         push!(layers, DepthwiseConv(
             kernel,
             num_channels=>num_channels,
             pad=padding,
-            relu
+            relu;
+            init=Flux.glorot_normal
         ))
     end
-    push!(layers, Conv(kernel_pointwise, num_channels=>out_channels))
+    push!(layers, Conv(
+        (1, 1),
+        num_channels=>out_channels;
+        init=Flux.glorot_normal
+    ))  # Pointwise matrix multiplication
 
     return (
         conv=Chain(layers...),
