853a823ae99f1679db0a48235044cc304c2b60a7
W.P. Bruinsma <wpb23@vartak.eng.cam.ac.uk>
2020-03-25 12:56:13 +0000

    Parametrise variance instead of std

--- train.jl
+++ train.jl
@@ -20,7 +20,7 @@ function plot_task(model, epoch)
 
     # Run model. Take care of the dimensionality of all objects.
     expand(x) = reshape(x, length(x), 1, 1)
-    y_mean, y_std = map(
+    y_mean, y_var = map(
         x -> Flux.data(cpu(x[:, 1, 1])),
         model(expand.((x_context, y_context, x))...)
     )
@@ -34,7 +34,7 @@ function plot_task(model, epoch)
     # Plot prediction
     plot!(plt, x, y_mean, c=:green, label="Model Output")
     plot!(plt, x, [y_mean y_mean],
-        fillrange=[y_mean .+ 2y_std y_mean .- 2y_std],
+        fillrange=[y_mean .+ 2 .* sqrt(y_var) y_mean .- 2 .* sqrt(y_var)],
         fillalpha=0.2,
         c=:green,
         label=""
@@ -64,15 +64,16 @@ data_gen = DataGenerator(
     max_target_points=50
 )
 
-init_conv(k, ch) = (Flux.glorot_normal(k..., ch...), fill(0.001f0, ch[2]))
+init_conv(k, ch) = (Flux.glorot_normal(k..., ch...), fill(1f-3, ch[2]))
 
 # Use the SimpleConv architecture.
 conv = Chain(
+    x -> sigmoid.(x),
     Conv(init_conv((1, 1), 2=>8)..., relu; pad=0),
     Conv(init_conv((5, 1), 8=>16)..., relu; pad=(2, 0)),
     Conv(init_conv((5, 1), 16=>32)..., relu; pad=(2, 0)),
     Conv(init_conv((5, 1), 32=>16)..., relu; pad=(2, 0)),
-    Conv(init_conv((5, 1), 16=>8)..., relu; pad=(2, 0)),
+    Conv(init_conv((5, 1), 16=>8)..., sigmoid; pad=(2, 0)),
     Conv(init_conv((1, 1), 8=>2)...; pad=0)
 )
 arch = (conv=conv, points_per_unit=32f0, multiple=1)
@@ -87,7 +88,7 @@ model = convcnp_1d(arch; margin = scale * 2) |> gpu
 eval_model(model)
 
 # Configure training.
-opt = ADAM(5e-4)
+opt = ADAM(1e-3)
 EPOCHS = 100
 TASKS_PER_EPOCH = 256
 
@@ -102,5 +103,5 @@ for epoch in 1:EPOCHS
     )
     println("Epoch done")
     eval_model(model; num_batches=128)
-    plot_task(model, epoch)
+    # plot_task(model, epoch)
 end
