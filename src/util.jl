7272fbeff50a35b6158b75567c4876634ea3d2c6
gordonjo <gordonjo@post.bgu.ac.il>
2020-03-25 11:47:34 +0000

    Adding epsilon to standard deviation in Gaussian log-likelihood

--- src/util.jl
+++ src/util.jl
@@ -79,9 +79,9 @@ Gaussian log-pdf.
 function gaussian_logpdf(x::AbstractArray, μ::AbstractArray, σ::AbstractArray)
     # Loop fusion was introducing indexing, which severly bottlenecks GPU computation, so
     # we roll out the computation like this.
-    z = (x .- μ) ./ σ
+    z = (x .- μ) ./ (σ .+ 1.0f-4)
     logconst = 1.837877f0
-    logdet = 2f0 .* log.(σ)
+    logdet = 2f0 .* log.(σ .+ 1.0f-4)
     quad = z .* z
     sum = logconst .+ logdet .+ quad
     return -0.5f0 .* sum
