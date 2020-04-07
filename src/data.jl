cccac007a8a8cfe85245bb083a85a63ffe800533
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-04-07 13:45:41 +0200

    Add BayesianConvCNP

--- src/data.jl
+++ src/data.jl
@@ -1,4 +1,4 @@
-export DataGenerator, Sawtooth
+export DataGenerator, Sawtooth, BayesianConvCNP
 
 """
     DataGenerator
@@ -156,4 +156,120 @@ function Base.rand(fs::FiniteSawtooth)
 
     # Add noise and return.
     return f .+ sqrt(fs.noise) .* randn(eltype(x), size(x)...)
+end
+
+"""
+    Bayesian ConvCNP
+
+Bayesian ConvCNP.
+
+# Fields
+- `receptive_field::Float32`: Width of the receptive field.
+- `num_layers::Integer`: Number of layers of the CNN, excluding an initial
+    and final pointwise convolutional layer to change the number of channels
+    appropriately.
+- `num_channels::Integer`: Number of channels of the CNN.
+- `points_per_unit::Float32=30f0`: Points per unit for the discretisation. See
+     `UniformDiscretisation1d`.
+"""
+struct BayesianConvCNP
+    receptive_field::Float32
+    num_layers::Integer
+    num_channels::Integer
+    points_per_unit::Float32
+
+    function BayesianConvCNP(;
+        receptive_field::Float32=1f0,
+        num_layers::Integer=4,
+        num_channels::Integer=6,
+        points_per_unit::Float32=30f0
+    )
+        new(receptive_field, num_layers, num_channels, points_per_unit)
+    end
+end
+
+"""
+    FiniteBayesianConvCNP{T<:Real}
+
+Finite-dimensional distribution of a `BayesianConvCNP` at particular inputs.
+
+# Fields
+- `x::Vector{T}`: Inputs.
+- `noise::T`: Noise variance.
+- `convcnp::BayesianConvCNP`: Corresponding `BayesianConvCNP`.
+"""
+struct FiniteBayesianConvCNP{T<:Real}
+    x::Vector{T}
+    noise::T
+    convcnp::BayesianConvCNP
+end
+
+"""
+    (s::BayesianConvCNP)(x, noise)
+
+Construct the finite-dimensional distribution of a `BayesianConvCNP` at inputs `x` and
+observation noise variance `noise`.
+
+# Arguments
+- `x`: Inputs.
+- `noise`: Noise variance.
+
+# Returns
+- `FiniteBayesianConvCNP`: Corresponding finite-dimensional distribution.
+"""
+(convcnp::BayesianConvCNP)(x, noise) = FiniteBayesianConvCNP(x, noise, convcnp)
+
+_init_conv(k, ch) = (
+    Flux.glorot_normal(k..., ch...),
+    1f-3 .* randn(Float32, ch[2])
+)
+
+_init_depthwiseconv(k, ch) = (
+    Flux.glorot_normal(k..., div(ch[2], ch[1]), ch[1]),
+    1f-3 .* randn(Float32, ch[2])
+)
+
+function Base.rand(fconvcnp::FiniteBayesianConvCNP)
+    # Contruct discretisation.
+    discretisation = UniformDiscretisation1d(
+        fconvcnp.convcnp.points_per_unit,
+        fconvcnp.convcnp.receptive_field / 2,
+        1
+    )
+    x_discretisation = discretisation(fconvcnp.x)
+    x_discretisation = reshape(x_discretisation, length(x_discretisation), 1, 1)
+
+    # Construct CNN with random initialisation.
+    conv = build_conv_1d(
+        fconvcnp.convcnp.receptive_field,
+        fconvcnp.convcnp.num_layers,
+        fconvcnp.convcnp.num_channels;
+        points_per_unit=fconvcnp.convcnp.points_per_unit,
+        in_channels=1,
+        out_channels=1,
+        init_conv=_init_conv,
+        init_depthwiseconv=_init_depthwiseconv
+    ).conv
+
+    # Construct decoder.
+    scale = 2 / fconvcnp.convcnp.points_per_unit
+    decoder = SetConv([log(scale)], false)
+
+    # Draw random encoding.
+    encoding = randn(Float32, length(x_discretisation))
+
+    # Pass through CNN, which takes in images of height one.
+    encoding = reshape(encoding, length(encoding), 1, 1, 1)
+    latent = conv(encoding)
+    latent = reshape(latent, length(latent), 1, 1)
+
+    # Perform decoding.
+    x_target = reshape(fconvcnp.x, length(fconvcnp.x), 1, 1)
+    sample = decoder(x_discretisation, latent, x_target)[:, 1, 1]
+
+    # Normalise sample.
+    sample = (sample .- mean(sample)) ./ std(sample)
+
+    # Return with noise.
+    return sample .+ sqrt(fconvcnp.noise) .* randn(Float32, size(sample)...)
 end
