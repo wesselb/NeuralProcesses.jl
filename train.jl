2b9ca85f41cd9fa3eab05e4172118598e5281196
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-05-01 22:22:35 +0200

    Add model selection option

--- train.jl
+++ train.jl
@@ -16,8 +16,12 @@ using Distributions
 # Parse command line arguments.
 parser = ArgParseSettings()
 @add_arg_table! parser begin
+    "--data"
+        help = "Data set."
+        arg_type = String
+        required = true
     "--model"
-        help = "Model to train or evaluate."
+        help = "Model."
         arg_type = String
         required = true
     "--starting-epoch"
@@ -35,48 +39,48 @@ end
 args = parse_args(parser)
 
 # Set up experiment.
-if args["model"] == "eq-small"
+if args["data"] == "eq-small"
     process = GP(stretch(eq(), 1 / 0.25), GPC())
+    num_context = DiscreteUniform(3, 50)
+    num_target = DiscreteUniform(3, 50)
     receptive_field = 1f0
-    channels = 16
-    num_context = DiscreteUniform(3, 50)
-    num_target = DiscreteUniform(3, 50)
     points_per_unit = 32f0
-elseif args["model"] == "eq"
+    num_channels = 16
+elseif args["data"] == "eq"
     process = GP(stretch(eq(), 1 / 0.25), GPC())
-    receptive_field = 2f0
-    channels = 64
     num_context = DiscreteUniform(3, 50)
     num_target = DiscreteUniform(3, 50)
+    receptive_field = 2f0
     points_per_unit = 64f0
-elseif args["model"] == "matern52"
+    num_channels = 64
+elseif args["data"] == "matern52"
     process = GP(stretch(matern52(), 1 / 0.25), GPC())
+    num_context = DiscreteUniform(3, 50)
+    num_target = DiscreteUniform(3, 50)
     receptive_field = 2f0
-    channels = 64
-    num_context = DiscreteUniform(3, 50)
-    num_target = DiscreteUniform(3, 50)
     points_per_unit = 64f0
-elseif args["model"] == "weakly-periodic"
+    num_channels = 64
+elseif args["data"] == "weakly-periodic"
     process = GP(stretch(eq(), 1 / 0.5) * stretch(Stheno.PerEQ(), 1 / 0.25), GPC())
-    receptive_field = 4f0
-    channels = 64
     num_context = DiscreteUniform(3, 50)
     num_target = DiscreteUniform(3, 50)
+    receptive_field = 4f0
     points_per_unit = 64f0
-elseif args["model"] == "sawtooth"
+    num_channels = 64
+elseif args["data"] == "sawtooth"
     process = Sawtooth()
-    receptive_field = 16f0
-    channels = 32
     num_context = DiscreteUniform(3, 100)
     num_target = DiscreteUniform(3, 100)
+    receptive_field = 16f0
     points_per_unit = 64f0
+    num_channels = 32
 else
-    error("Unknown model \"$model\".")
+    error("Unknown data \"" * args["data"] * "\".")
 end
 
 # Determine name of file to write model to and folder to output images.
-bson = "models/" * args["model"] * ".bson"
-path = "output/" * args["model"]
+bson = "models/" * args["model"] * "/" * args["data"] * ".bson"
+path = "output/" * args["model"] * "/" * args["data"]
 
 # Ensure that the directories exist.
 mkpath("models")
@@ -99,21 +103,36 @@ elseif args["starting-epoch"] > 1
     model = recent_model(bson)
 else
     # Instantiate a new model to start training.
-    arch = build_conv(
-        receptive_field,
-        8,
-        channels,
-        points_per_unit=points_per_unit,
-        dimensionality=1
-    )
-    model = convcnp_1d(arch; margin=receptive_field) |> gpu
+    if args["model"] == "convcnp"
+        model = convcnp_1d(
+            receptive_field=receptive_field,
+            num_layers=8,
+            num_channels=num_channels,
+            points_per_unit=points_per_unit,
+            margin=receptive_field
+        )
+        loss_args = ()
+    elseif args["model"] == "convnp"
+        model = convnp_1d(
+            receptive_field=receptive_field,
+            num_layers=8,
+            encoder_channels=num_channels,
+            decoder_channels=num_channels,
+            latent_channels=2,
+            points_per_unit=points_per_unit,
+            margin=receptive_field
+        )
+        loss_args = (5,)
+    else
+        error("Unknown model \"" * args["model"] * "\".")
+    end
 end
 
 # Report number of parameters.
 println("Number of parameters: ", sum(map(length, Flux.params(model))))
 
 if args["evaluate"]
-    eval_model(model, data_gen, 100, num_batches=10000)
+    eval_model(model, data_gen, 100, num_batches=10000, loss_args=loss_args)
 else
     train!(
         model,
@@ -123,6 +142,7 @@ else
         batches_per_epoch=2048,
         starting_epoch=args["starting-epoch"],
         epochs=args["epochs"],
-        path=path
+        path=path,
+        loss_args=loss_args
     )
 end
