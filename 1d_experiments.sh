e7c823b94b6f5ec9b2797f184c4902b984475627
W.P. Bruinsma <wpb23@vartak.eng.cam.ac.uk>
2020-05-01 23:08:36 +0100

    Also loop over models

--- 1d_experiments.sh
+++ 1d_experiments.sh
@@ -2,33 +2,41 @@
 
 set -e
 
+MODELS="convcnp convnp"
 DATA_SETS="eq matern52 weakly-periodic sawtooth"
 EPOCHS_INC=20
 EPOCHS_TOTAL=100
 
-EPOCHS_INC_PLUS_ONE=$(echo $EPOCHS_INC + 1 | bc)
-
 JULIA=${1:-julia}
-BIN="$JULIA train.jl --epochs $EPOCHS_INC --model convcnp"
 
-echo Binary: $BIN
+EPOCHS_INC_PLUS_ONE=$(echo $EPOCHS_INC + 1 | bc)
+BIN="$JULIA train.jl --epochs $EPOCHS_INC"
+
+echo ---------------------------
+echo Binary:    $BIN
+echo Models:    $MODELS
 echo Data sets: $DATA_SETS
+echo ---------------------------
 
-# First train all models.
-for data in $DATA_SETS
+for model in $MODELS
 do
-    echo Data set: $data
-    $BIN --data $data --starting-epoch 1
-    for starting_epoch in $(seq $EPOCHS_INC_PLUS_ONE $EPOCHS_INC $EPOCHS_TOTAL)
+    for data in $DATA_SETS
     do
-        echo Resuming from epoch $starting_epoch
-        $BIN --data $data --starting-epoch $starting_epoch
+        echo Data set: $data
+        $BIN --data $data --model $model --starting-epoch 1
+        for starting_epoch in $(seq $EPOCHS_INC_PLUS_ONE $EPOCHS_INC $EPOCHS_TOTAL)
+        do
+            echo Resuming from epoch $starting_epoch
+            $BIN --data $data --model $model --starting-epoch $starting_epoch
+        done
     done
-done
+end
 
-# Finally evaluate all models.
-for data in $DATA_SETS
+for model in $MODELS
 do
-    echo Data set: $data
-    $BIN --data $data --evaluate
-done
+    for data in $DATA_SETS
+    do
+        echo Data set: $data
+        $BIN --data $data --model $model --evaluate
+    done
+doen
