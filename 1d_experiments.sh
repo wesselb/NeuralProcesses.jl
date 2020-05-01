9cdcdec9259aaca4f457c8b2887267256c42b8d1
W.P. Bruinsma <wpb23@vartak.eng.cam.ac.uk>
2020-05-01 09:56:27 +0100

    Evaluate all models at the end

--- 1d_experiments.sh
+++ 1d_experiments.sh
@@ -8,6 +8,7 @@ SETTINGS="--epochs 20"
 BIN="$JULIA train.jl $SETTINGS"
 echo Binary: $BIN
 
+# First train all models.
 for model in eq matern52 weakly-periodic sawtooth
 do
     echo Model: $model
@@ -20,6 +21,12 @@ do
         echo Resuming from epoch $starting_epoch
         $BIN --model $model --starting-epoch $starting_epoch
     done
+done
+
+# Finally evaluate all models.
+for model in eq matern52 weakly-periodic sawtooth
+do
+    echo Model: $model
 
     echo Evaluating model
     $BIN --model $model --evaluate
