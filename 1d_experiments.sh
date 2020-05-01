0383bb4865b312a385e513cfe0fd559d0aa6219e
W.P. Bruinsma <wpb23@vartak.eng.cam.ac.uk>
2020-05-01 15:56:24 +0100

    Improve experiment script

--- 1d_experiments.sh
+++ 1d_experiments.sh
@@ -2,21 +2,24 @@
 
 set -e
 
-JULIA=${1:-julia}
-SETTINGS="--epochs 20"
+MODELS=(eq matern52 weakly-periodic sawtooth)
+EPOCHS_INC=20
+EPOCHS_TOTAL=100
 
-BIN="$JULIA train.jl $SETTINGS"
+EPOCHS_INC_PLUS_ONE=$(echo $EPOCHS_INC + 1 | bc)
+
+JULIA=${1:-julia}
+BIN="$JULIA train.jl --epochs $EPOCHS_INC"
+
 echo Binary: $BIN
+echo Models: $MODELS
 
 # First train all models.
-for model in eq matern52 weakly-periodic sawtooth
+for model in $MODELS
 do
-    echo Model: $model
-    
-    echo Training model
+    echo Training model: $model
     $BIN --model $model --starting-epoch 1
-
-    for starting_epoch in $(seq 21 20 100)
+    for starting_epoch in $(seq $EPOCHS_INC_PLUS_ONE $EPOCHS_INC $EPOCHS_TOTAL)
     do
         echo Resuming from epoch $starting_epoch
         $BIN --model $model --starting-epoch $starting_epoch
@@ -24,10 +27,8 @@ do
 done
 
 # Finally evaluate all models.
-for model in eq matern52 weakly-periodic sawtooth
+for model in $MODELS
 do
-    echo Model: $model
-
-    echo Evaluating model
+    echo Evaluating model: $model
     $BIN --model $model --evaluate
 done
