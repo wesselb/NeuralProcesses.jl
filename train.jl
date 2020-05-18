cb75152ac7157d5f126b8fdadab28384e2d716be
W.P. Bruinsma <wpb23@vartak.eng.cam.ac.uk>
2020-05-18 17:49:31 +0100

    Up learning rate

--- train.jl
+++ train.jl
@@ -259,7 +259,7 @@ else
         model,
         loss,
         data_gen,
-        ADAM(1e-4),
+        ADAM(5e-4),
         bson=bson,
         starting_epoch=args["starting-epoch"],
         tasks_per_epoch=2^14,
