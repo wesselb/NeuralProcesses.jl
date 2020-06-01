577172e212c22a91494d56790e41a2bfa353d955
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-06-01 13:19:47 +0200

    Show epochs setup

--- train.sh
+++ train.sh
@@ -6,6 +6,9 @@ JULIA="${JULIA:-julia}"
 EPOCHS_INC="${EPOCHS_INC:-10}"
 EPOCHS_TOTAL="${EPOCHS_TOTAL:-100}"
 
+echo "Epochs total:     $EPOCHS_TOTAL"
+echo "Epochs increment: $EPOCHS_INC"
+
 $JULIA --project=. train.jl --starting-epoch 1 --epochs $EPOCHS_INC $@
 for starting_epoch in $(seq $(echo $EPOCHS_INC + 1 | bc) $EPOCHS_INC $EPOCHS_TOTAL)
 do
