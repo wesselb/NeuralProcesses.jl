14df8112ac243e319aca4829f08144c487da8fd2
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2020-07-30 10:18:23 +0200

    Fix order of data sets

--- train_all.sh
+++ train_all.sh
@@ -2,7 +2,7 @@
 
 for loss in loglik elbo loglik-iw; do
     for model in cnp acnp convcnp np-het anp-het convnp-het; do
-        for data in matern52 eq noisy-mixture weakly-periodic sawtooth mixture; do
+        for data in eq matern52 noisy-mixture weakly-periodic sawtooth mixture; do
             ./train.sh --model $model --loss $loss --data $data \
                 2>&1 | tee logs/${model}_${loss}_${data}.log
         done
