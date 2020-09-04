1b1ba4b13edd18738bc138880a44e20cba6dc70b
W.P. Bruinsma <wpb23@vartak.eng.cam.ac.uk>
2020-09-04 13:29:59 +0100

    Add script to eval all

---
+++ eval_all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+for loss in loglik elbo loglik-iw; do
+    for model in cnp acnp convcnp np-het anp-het convnp-het; do
+        for data in eq matern52 noisy-mixture weakly-periodic sawtooth mixture; do
+            ./eval.sh --model $model --loss $loss --data $data --evaluate-only-within \
+                2>&1 | tee logs/eval_${model}_${loss}_${data}.log
+        done
+    done
+done
